ccb83bfba601b975019d18ecf9d48fe8e87f7722
purplepig4657 <80941516+purplepig4657@users.noreply.github.com>
2022-02-26 01:56:46 +0900

    baekjoon 2011, 2302

--- baekjoon/dp/cipher_code.py
+++ baekjoon/dp/cipher_code.py
@@ -3,24 +3,25 @@ Baekjoon 2011 암호코드
 
 """
 
-cipher = list(map(int, input()))
-cipher.insert(0, 0)
-dp = [0 for _ in range(len(cipher))]
+if __name__ == '__main__':
+    cipher = list(map(int, input()))
+    cipher.insert(0, 0)
+    dp = [0 for _ in range(len(cipher))]
 
-dp[0] = 1
+    dp[0] = 1
 
-if cipher[1] != 0:
-    dp[1] = 1
+    if cipher[1] != 0:
+        dp[1] = 1
 
-for i in range(2, len(cipher)):
-    cipher1 = cipher[i - 1]
-    cipher2 = cipher[i]
-    cipher1_2 = cipher1 * 10 + cipher2
+    for i in range(2, len(cipher)):
+        cipher1 = cipher[i - 1]
+        cipher2 = cipher[i]
+        cipher1_2 = cipher1 * 10 + cipher2
 
-    if cipher2 != 0:
-        dp[i] += dp[i - 1] % 1000000
+        if cipher2 != 0:
+            dp[i] += dp[i - 1] % 1000000
 
-    if 0 < cipher1_2 <= 26 and cipher1 != 0:
-        dp[i] += dp[i - 2] % 1000000
+        if 0 < cipher1_2 <= 26 and cipher1 != 0:
+            dp[i] += dp[i - 2] % 1000000
 
-print(dp[len(cipher) - 1] % 1000000)
+    print(dp[len(cipher) - 1] % 1000000)
---
+++ baekjoon/dp/theater_seats.py
@@ -0,0 +1,25 @@
+def fibonacci(n):
+    if n == 0:
+        return 1
+    dp = [0] * (n + 1)
+    dp[0] = 1
+    dp[1] = 1
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+if __name__ == '__main__':
+    N = int(input())
+    M = int(input())
+    vip = [0] * (M + 1)
+    for i in range(1, M + 1):
+        vip[i] = int(input())
+    vip.append(N + 1)
+
+    answer = 1
+    for i in range(1, M + 2):
+        answer *= fibonacci(vip[i] - vip[i - 1] - 1)
+
+    print(answer)
